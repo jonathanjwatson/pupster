bf2097c249f99f73f7586275586a57233a622581
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-07-31 13:47:59 -0400

    Updates

---
+++ precache-manifest.e07dccbea9f1441d8688439f657af7be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d375f742879764ae3337d3de5c8562ab",
+    "url": "/pupster/index.html"
+  },
+  {
+    "revision": "02560eb43892b95eaccf",
+    "url": "/pupster/static/js/2.f0fb4828.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pupster/static/js/2.f0fb4828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e10041248d0ba687e5",
+    "url": "/pupster/static/js/main.a1691bc2.chunk.js"
+  },
+  {
+    "revision": "08e668c994ed8d910483",
+    "url": "/pupster/static/js/runtime-main.2d8bf639.js"
+  }
+]);
